1ccfa9b46a816904e9a0c91f02940eb5e6e7b823
Aashish Paliwal <aashish.paliwal@live.com>
2024-11-24 16:06:03 +0530

    add paymentChannel contract (for hash chain)

---
+++ contracts/paymenChannel.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import "./utility.sol";
+
+contract paymenChannel {
+    Utility public utility;
+
+    struct Channel {
+        bytes32 trustAnchor;
+        uint256 amount;
+        uint256 numberOfTokens;
+        uint256 withdrawAfterBlocks;
+    }
+
+    // Nested mapping to store channels: user => merchant => Channel
+    mapping (address => mapping (address => Channel)) public channelsMapping;
+
+    constructor(address utilityAddress) {
+        utility = Utility(utilityAddress);
+    }
+
+    function createChannel (address merchant, bytes32 trustAnchor, uint256 amount, uint256 numberOfTokens, uint256 withdrawAfterBlocks) public payable {
+        require (msg.value == amount, "incorrect amount sent.");
+        channelsMapping[msg.sender][merchant] = Channel ({
+            trustAnchor: trustAnchor,
+            amount: amount,
+            numberOfTokens: numberOfTokens,
+            withdrawAfterBlocks: withdrawAfterBlocks
+        });
+    }
+
+    function withdrawChannel(address payer, bytes32 finalHashValue, uint256 numberOfTokensUsed) public {
+        Channel storage channel = channelsMapping[payer][msg.sender]; // Use storage to update state directly
+        require(channel.amount > 0, "Channel does not exist or has been withdrawn.");
+        
+        require(utility.verifyHashchain(channel.trustAnchor, finalHashValue, numberOfTokensUsed), "Verification failed.");
+
+        uint256 payableAmount = (channel.amount * numberOfTokensUsed) / channel.numberOfTokens;
+        require(payableAmount > 0, "No amount is payable.");
+        delete channelsMapping[payer][msg.sender];
+
+        (bool sent, ) = payable(msg.sender).call{value: payableAmount}("");
+        require(sent, "Failed to send Ether");
+    }
+}
